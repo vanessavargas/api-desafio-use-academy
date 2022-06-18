e1bdaec61c8fa5bdef0abf05ec60e90339d8e0b4
Vanessa Vargas <vanessa.vargas4f@gmail.com>
2022-06-18 00:06:53 -0300

    install and config cors

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,17 @@ import express, { Request, Response } from 'express';
 import { routes } from './routes';
 import { env } from './config/environment-variables';
 import { AppDataSource } from './config/data-source';
+import cors from 'cors';
 
 const PORT = env.PORT || 3000;
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  }),
+);
 
 app.use(routes);
 
